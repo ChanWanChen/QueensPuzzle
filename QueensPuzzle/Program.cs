4dc9a44f2329b1b13a94ff4f50fcc54e6148d862
詹宛真 <aquarius4201@gmail.com>
2021-04-26 17:43:27 +0800

    start

---
+++ QueensPuzzle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueensPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
